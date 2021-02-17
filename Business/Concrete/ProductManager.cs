582598bf1f62c986a2c25d8def8ccb898e3028cb
Mina <33224492+Minazor@users.noreply.github.com>
2021-02-18 00:11:59 +0300

    AOP giriş

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -23,14 +24,10 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        [Validate]
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
-            
-            /********************Validation***********************/
-            ValidationTool.Validate(new ProductValidator(),product);
-            /****************************************************/
-
+           
             //business codes
             _productDal.Add(product);
 
